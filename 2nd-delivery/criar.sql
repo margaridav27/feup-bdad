67105a10da75615e0e07dfeeae527f330064463a
Miguel <migb.rodrigues@gmail.com>
2021-03-26 19:25:50 +0000

    Changed some constraints in oreder to match with sqlite standard

--- 2nd-delivery/criar.sql
+++ 2nd-delivery/criar.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS Organization (
 CREATE TABLE IF NOT EXISTS OrganizationMemberVisibility (
     user TEXT,
     organization TEXT,
-    isPrivate INT CHECK (isPrivate >= 0 AND isPrivate <= 1),
+    isPrivate INTEGER CHECK (isPrivate >= 0 AND isPrivate <= 1),
     CONSTRAINT OrganizationMemberVisibilityPK PRIMARY KEY (user, organization),
     CONSTRAINT OrganizationMemberVisibilityUserFK FOREIGN KEY (user) REFERENCES User(name),
     CONSTRAINT OrganizationMemberVisibilityOrganizationFK FOREIGN KEY (organization) REFERENCES Organization(name)
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS Team (
 CREATE TABLE IF NOT EXISTS TeamRole (
     user TEXT,
     team TEXT,
-    isMaintainer INT CHECK (isMaintainer >= 0 AND isMaintainer <= 1),
+    isMaINTEGERainer INTEGER CHECK (isMaINTEGERainer >= 0 AND isMaINTEGERainer <= 1),
     CONSTRAINT TeamRolePK PRIMARY KEY (user, team),
     CONSTRAINT TeamRoleUserFK FOREIGN KEY (user) REFERENCES User(name),
     CONSTRAINT TeamRoleTeamFK FOREIGN KEY (team) REFERENCES Team(name)
@@ -54,19 +54,17 @@ CREATE TABLE IF NOT EXISTS TeamRole (
 /* Repos & Stuff */
 
 CREATE TABLE IF NOT EXISTS Repository (
-    ID INT UNIQUE CHECK (ID >= 1),
+    ID INTEGER UNIQUE CHECK (ID >= 1),
     name TEXT NOT NULL,
-    rootDirectory INT,
-    defaultBranch TEXT,
-    isVisible INT CHECK (isVisible >= 0 AND isVisible <= 1),
+    rootDirectory INTEGER,
+    isVisible INTEGER CHECK (isVisible >= 0 AND isVisible <= 1),
     CONSTRAINT RepositoryPK PRIMARY KEY (ID),
-    CONSTRAINT RepositoryRootFK FOREIGN KEY (rootDirectory) REFERENCES Directory(ID),
-    CONSTRAINT RepositoryDefaultBranchFK FOREIGN KEY (defaultBranch) REFERENCES Branch(name)
+    CONSTRAINT RepositoryRootFK FOREIGN KEY (rootDirectory) REFERENCES Directory(ID)
 );
 
 CREATE TABLE IF NOT EXISTS Submodule (
-    source INT,
-    destination INT, 
+    source INTEGER,
+    destination INTEGER, 
     CONSTRAINT SubmodulePK PRIMARY KEY (source, destination),
     CONSTRAINT SubmoduleSourceFK FOREIGN KEY (source) REFERENCES Repository(ID),
     CONSTRAINT SubmoduleDestinationFK FOREIGN KEY (destination) REFERENCES Repository(ID)
@@ -74,7 +72,7 @@ CREATE TABLE IF NOT EXISTS Submodule (
 
 CREATE TABLE IF NOT EXISTS OwnerRepository (
     user TEXT,
-    repository INT,
+    repository INTEGER,
     CONSTRAINT OwnerRepositoryPK PRIMARY KEY (user, repository),
     CONSTRAINT OwnerRepositoryUserFK FOREIGN KEY (user) REFERENCES User(name),
     CONSTRAINT OwnerRepositoryRepositoryFK FOREIGN KEY (repository) REFERENCES Repository(ID)
@@ -82,7 +80,7 @@ CREATE TABLE IF NOT EXISTS OwnerRepository (
 
 CREATE TABLE IF NOT EXISTS ContributorRepository (
     user TEXT,
-    repository INT,
+    repository INTEGER,
     CONSTRAINT ContributorRepositoryPK PRIMARY KEY (user, repository),
     CONSTRAINT ContributorRepositoryUserFK FOREIGN KEY (user) REFERENCES User(name),
     CONSTRAINT ContributorRepositoryRepositoryFK FOREIGN KEY (repository) REFERENCES Repository(ID)
@@ -90,7 +88,7 @@ CREATE TABLE IF NOT EXISTS ContributorRepository (
 
 CREATE TABLE IF NOT EXISTS TeamRepository (
     team TEXT,
-    repository INT,
+    repository INTEGER,
     CONSTRAINT TeamRepositoryPK PRIMARY KEY (team, repository),
     CONSTRAINT TeamRepositoryTeamFK FOREIGN KEY (team) REFERENCES Team(name),
     CONSTRAINT TeamRepositoryRepositoryFK FOREIGN KEY (repository) REFERENCES Repository(ID)
@@ -98,7 +96,7 @@ CREATE TABLE IF NOT EXISTS TeamRepository (
 
 CREATE TABLE IF NOT EXISTS OrganizationRepository (
     organization TEXT,
-    repository INT,
+    repository INTEGER,
     CONSTRAINT OrganizationRepositoryPK PRIMARY KEY (organization, repository),
     CONSTRAINT OrganizationRepositoryOrganizationFK FOREIGN KEY (organization) REFERENCES Organization(name),
     CONSTRAINT OrganizationRepositoryRepositoryFK FOREIGN KEY (repository) REFERENCES Repository(ID)
@@ -109,13 +107,14 @@ CREATE TABLE IF NOT EXISTS OrganizationRepository (
 
 CREATE TABLE IF NOT EXISTS Branch (
     name TEXT DEFAULT "main" NOT NULL,
-    repository INT,
+    isDefault INTEGER CHECK (isDefault >= 0 AND isDefault <= 1),
+    repository INTEGER,
     CONSTRAINT BranchPK PRIMARY KEY (name, repository),
     CONSTRAINT BranchRepositoryFK FOREIGN KEY (repository) REFERENCES Repository(ID)
 );
 
 CREATE TABLE IF NOT EXISTS "Commit" (
-    ID INT, 
+    ID INTEGER, 
     commitHash TEXT NOT NULL CHECK (LENGTH(commitHash) == 40),
     message TEXT,
     CONSTRAINT CommitPK PRIMARY KEY (ID),
@@ -124,7 +123,7 @@ CREATE TABLE IF NOT EXISTS "Commit" (
 
 CREATE TABLE IF NOT EXISTS Tag (
     name TEXT NOT NULL,
-    "commit" INT,
+    "commit" INTEGER,
     CONSTRAINT TagPK PRIMARY KEY (name, "commit"),
     CONSTRAINT TagCommitFK FOREIGN KEY ("commit") REFERENCES "Commit"(ID)
 );
@@ -132,9 +131,9 @@ CREATE TABLE IF NOT EXISTS Tag (
 /* Contributions */
 
 CREATE TABLE IF NOT EXISTS Contribution (
-    ID INT UNIQUE CHECK (ID >= 1),
+    ID INTEGER UNIQUE CHECK (ID >= 1),
     user TEXT,
-    repository INT,
+    repository INTEGER,
     date DATE,
     CONSTRAINT ContributionPK PRIMARY KEY (ID),
     CONSTRAINT ContributionUserFK FOREIGN KEY (user) REFERENCES User(name),
@@ -142,30 +141,30 @@ CREATE TABLE IF NOT EXISTS Contribution (
 );
 
 CREATE TABLE IF NOT EXISTS PullRequest (
-    ID INT,
+    ID INTEGER,
     /* How to calculate this number based on the Date from Contribution? */
-    pullRequestNumber INT CHECK (pullRequestNumber >= 1),
-    status INT CHECK (status >= 0 AND status <= 1),
+    pullRequestNumber INTEGER CHECK (pullRequestNumber >= 1),
+    status INTEGER CHECK (status >= 0 AND status <= 1),
     message TEXT,
     CONSTRAINT PullRequestPK PRIMARY KEY (ID),
     CONSTRAINT PullRequestContributionFK FOREIGN KEY (ID) REFERENCES Contribution(ID)
 );
 
 CREATE TABLE IF NOT EXISTS Issue (
-    ID INT,
+    ID INTEGER,
     /* How to calculate this number based on the Date from Contribution? */
-    issueNumber INT CHECK (issueNumber >= 1),
+    issueNumber INTEGER CHECK (issueNumber >= 1),
     message TEXT,
     CONSTRAINT IssuePK PRIMARY KEY (ID),
     CONSTRAINT IssueContributionFK FOREIGN KEY (ID) REFERENCES Contribution(ID)
 );
 
 CREATE TABLE IF NOT EXISTS Merge (
-    ID INT,
+    ID INTEGER,
     oursName TEXT,
-    oursRepository INT,
+    oursRepository INTEGER,
     theirsName TEXT,
-    theirsRepository INT,
+    theirsRepository INTEGER,
     CONSTRAINT MergePK PRIMARY KEY (ID),
     CONSTRAINT MergeCommitFK FOREIGN KEY (ID) REFERENCES "Commit"(ID),
     CONSTRAINT MergeBranchOursNameFK FOREIGN KEY (oursName, oursRepository) REFERENCES Branch(name, repository),
@@ -180,7 +179,7 @@ CREATE TABLE IF NOT EXISTS Merge (
 
 CREATE TABLE IF NOT EXISTS Directory (
     /* Limit the number of chars for a directory imposed in UNIX */
-    ID INT UNIQUE CHECK (ID >= 1),
+    ID INTEGER UNIQUE CHECK (ID >= 1),
     name TEXT NOT NULL CHECK (LENGTH(name) <= 255),
     CONSTRAINT DirectoryPK PRIMARY KEY (ID)
 );
@@ -188,7 +187,7 @@ CREATE TABLE IF NOT EXISTS Directory (
 CREATE TABLE IF NOT EXISTS File (
     /* Limit the number of chars for a file imposed in UNIX */
     name TEXT NOT NULL CHECK (LENGTH(name) <= 255),
-    directory INT,
+    directory INTEGER,
     content TEXT,
     programmingLanguage TEXT,
     CONSTRAINT FilePK PRIMARY KEY (name, directory),
@@ -202,8 +201,8 @@ CREATE TABLE IF NOT EXISTS ProgrammingLanguage (
 );
 
 CREATE TABLE IF NOT EXISTS FolderRelationship (
-    parent INT,
-    child INT, 
+    parent INTEGER,
+    child INTEGER,
     CONSTRAINT FolderRelationshipPK PRIMARY KEY (parent, child),
     CONSTRAINT FolderRelationshipParentFK FOREIGN KEY (parent) REFERENCES Directory(ID),
     CONSTRAINT FolderRelationshipChildFK FOREIGN KEY (child) REFERENCES Directory(ID)
