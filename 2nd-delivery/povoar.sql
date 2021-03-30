9c32a78f2b1efce552b8ff1fd1ea0ef0396dbc0b
Margarida Vieira <margaridajcv.27@gmail.com>
2021-03-30 22:54:02 +0100

    added some directories

--- 2nd-delivery/povoar.sql
+++ 2nd-delivery/povoar.sql
@@ -50,3 +50,29 @@ VALUES
     ("webappDist"),
     ("youtube"),
     ("mmarket");
+
+INSERT INTO Directory (ID, name)
+VALUES
+    (1, "feup-lpoo")
+    (2, "feup-sope")
+    (3, "feup-bdad")
+    (4, "feup-cgra")
+    (5, "hero")
+    (6, "MindAPI")
+    (7, "codejam2021")
+    (8, "bitcoin")
+    (9, "go-ethereum")
+    (10, "covid19-growth")    
+    (11, "advent-of-code-2019")
+    (12, "website-NIAEFEUP")
+    (13, "uporto-timetable-scrapper")
+    (14, "linux")
+    (15, "hackerrank-contest")
+    (16, "iio-oscilloscope")
+    (17, "project-schrodinger")
+    (18, "holidata.net")
+    (19, "timewarrior")
+    (20, "gbf.org")
+    (21, "GothenburgBitFactory")
+    (22, "cognitivebing")
+    
