ee816a80e479662a4f6a541a137ab3aa27503449
beatriz-ag <beatrizaguiar.up@gmail.com>
2021-03-31 17:21:19 +0100

    Added directories and branches

--- 2nd-delivery/povoar.sql
+++ 2nd-delivery/povoar.sql
@@ -263,6 +263,55 @@ VALUES
     (33, "workshop-LaTex"),
     (34, "feup-laig"),
     (35, "proj-xmod-tool");
+    (35, "TP1");
+    (36, "TP2");
+    (38, "TP3";
+    (39, "TP4");
+    (37, "TP5");
+    (40, "TP6");
+    (41, "TP7");
+    (42, "TP8");
+    (43, "TP9");
+    (44, "TP10");
+    (45, "QUIZZ");
+    (46, "src");
+    (47, "build");
+    (48, "bin");
+    (49, "Graddle");
+    (51, "PROJS");
+    (52, "PROJ1");
+    (53, "PROJ2");
+    (54, "PROJ3");
+    (55, "PROJ4");
+    (56, "Timetables");
+    (57, "tests");
+    (58, "bitcoinslib");
+    (59, "bitcoinArmory");
+    (60, "security");
+    (61, "sound");
+    (62, "graphics");
+    (63, "blocks");
+    (64, "LICENSES");
+    (65, "drivers");
+    (66, "Docs");
+    (67, "kernel");
+    (68, "tools");
+    (69, "scripts");
+    (70, "proteins");
+    (71, "vaccine");
+    (72, "data");
+    (73, "models");
+    (74, "Systems");
+    (75, "Shaders");
+    (76, "Effects");
+    (77, "Engines");
+    (78, "quickstart");
+    (79, "config");
+
+
+
+
+
 
 
 INSERT INTO ProgrammingLanguage (name)
@@ -374,4 +423,154 @@ VALUES
     ("main", 33, 1),
     ("main", 34, 1),
     ("main", 35, 1);
-    
+    ("main", 36, 1);
+    ("main", 37, 1);
+    ("main", 38, 1);
+    ("main", 39, 1);
+    ("main", 40, 1);
+    ("main", 41, 1);
+    ("main", 42, 1);
+    ("main", 43, 1);
+    ("main", 44, 1);
+    ("main", 45, 1);
+    ("main", 46, 1);
+    ("main", 47, 1);
+    ("main", 48, 1);
+    ("main", 49, 1);
+    ("main", 50, 1);
+    ("main", 51, 1);
+    ("main", 52, 1);
+    ("main", 53, 1);
+    ("main", 54, 1);
+    ("main", 55, 1);
+    ("main", 56, 1);
+    ("main", 57, 1);
+    ("main", 58, 1);
+    ("main", 59, 1);
+    ("main", 60, 1);
+    ("main", 61, 1);
+    ("main", 62, 1);
+    ("main", 63, 1);
+    ("main", 64, 1);
+    ("main", 65, 1);
+    ("main", 66, 1);
+    ("main", 67, 1);
+    ("main", 68, 1);
+    ("main", 69, 1);
+    ("main", 70, 1);
+    ("main", 71, 1);
+    ("main", 72, 1);
+    ("main", 73, 1);
+    ("main", 74, 1);
+    ("main", 75, 1);
+    ("main", 76, 1);
+    ("main", 77, 1);
+    ("main", 78, 1);
+    ("main", 79, 1);
+    ("main", 80, 1);
+    ("development", 1, 1),
+    ("development", 2, 1),
+    ("development", 3, 1),
+    ("development", 4, 1),
+    ("development", 5, 1),
+    ("development", 6, 1),
+    ("development", 7, 1),
+    ("development", 8, 1),
+    ("development", 9, 1),
+    ("development", 10, 1),    
+    ("development", 11, 1),
+    ("development", 12, 1),
+    ("development", 13, 1),
+    ("development", 14, 1),
+    ("development", 15, 1),
+    ("development", 16, 1),
+    ("development", 17, 1),
+    ("development", 18, 1),
+    ("development", 19, 1),
+    ("development", 20, 1),
+    ("development", 21, 1),
+    ("development", 22, 1),
+    ("development", 23, 1),
+    ("development", 24, 1),
+    ("development", 25, 1),
+    ("development", 26, 1),
+    ("development", 27, 1),
+    ("development", 28, 1),
+    ("development", 29, 1),
+    ("development", 30, 1),
+    ("development", 31, 1),
+    ("development", 32, 1),
+    ("development", 33, 1),
+    ("development", 34, 1),
+    ("development", 35, 1);
+    ("development", 36, 1);
+    ("development", 37, 1);
+    ("development", 38, 1);
+    ("development", 39, 1);
+    ("development", 40, 1);
+    ("development", 41, 1);
+    ("development", 42, 1);
+    ("development", 43, 1);
+    ("development", 44, 1);
+    ("development", 45, 1);
+    ("development", 46, 1);
+    ("development", 47, 1);
+    ("development", 48, 1);
+    ("development", 49, 1);
+    ("development", 50, 1);
+    ("development", 51, 1);
+    ("development", 52, 1);
+    ("development", 53, 1);
+    ("development", 54, 1);
+    ("development", 55, 1);
+    ("development", 56, 1);
+    ("development", 57, 1);
+    ("development", 58, 1);
+    ("development", 59, 1);
+    ("development", 60, 1);
+    ("development", 61, 1);
+    ("development", 62, 1);
+    ("development", 63, 1);
+    ("development", 64, 1);
+    ("development", 65, 1);
+    ("development", 66, 1);
+    ("development", 67, 1);
+    ("development", 68, 1);
+    ("development", 69, 1);
+    ("development", 70, 1);
+    ("development", 71, 1);
+    ("development", 72, 1);
+    ("development", 73, 1);
+    ("development", 74, 1);
+    ("development", 75, 1);
+    ("development", 76, 1);
+    ("development", 77, 1);
+    ("development", 78, 1);
+    ("development", 79, 1);
+    ("development", 80, 1);
+    ("feature-api", 3, 1);
+    ("feature-kernel", 6, 1);
+    ("testing-generator", 23, 1);
+    ("testing-xmod", 53, 1);
+    ("feature-getFlags", 65, 1);
+    ("testing-bitcoin", 23, 1);
+    ("feature-bits", 12, 1);
+    ("feature-spaceship", 54, 1);
+    ("testing-fly", 52, 1);
+    ("feature-coviddata", 12, 1);
+    ("testing-bulletproff", 76, 1);
+    ("testing-timetableOverlaps", 4, 1);
+    ("testing-tests", 8, 1);
+    ("feature-dataOrganizer", 45, 1);
+    ("feature-compiler", 98, 1);
+    ("testing-growth", 23, 1);
+    ("testing-AI", 12, 1);
+    ("testing-randomgenerator", 1, 1);
+    ("testing-move", 73, 1);
+    ("feature-jumpLoop", 66, 1);
+    ("feature-gbfST", 23, 1);
+    ("testing-display", 87, 1);
+    ("testing-shaders", 67, 1);
+    ("testing-usrLe", 78, 1);
+    ("testing-robotsL", 65, 1);
+    ("feature-mindT", 49, 1);
