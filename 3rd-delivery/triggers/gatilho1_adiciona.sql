42e0403ff6ff1f85cc162784a092b6b505a41b0c
Margarida Vieira <margaridajcv.27@gmail.com>
2021-05-16 18:28:21 +0100

    gatilho3 done

--- 3rd-delivery/triggers/gatilho1_adiciona.sql
+++ 3rd-delivery/triggers/gatilho1_adiciona.sql
@@ -1,1 +1,11 @@
 /* Aquando da criação de um repositório, criar o respetivo root directory*/
+DROP TRIGGER IF EXISTS CreateRootDirectoryForRepository;
+CREATE TRIGGER CreateRootDirectoryForRepository
+BEFORE INSERT ON Repository
+BEGIN
+    SELECT MAX(Directory.ID) FROM Directory AS lastDirectoryID;
+    INSERT INTO Directory (ID, "name") VALUES (lastDirectoryID + 1, New."name");
+
+    SELECT MAX(Directory.ID) FROM Directory AS lastDirectoryID;
+    UPDATE Repository SET rootDirectory = lastDirectoryID;
+END;
--- 3rd-delivery/triggers/gatilho3_adiciona.sql
+++ 3rd-delivery/triggers/gatilho3_adiciona.sql
@@ -1,2 +1,18 @@
-/* Nome de uma tag é único no contexto de um repositório. 
- Raise(ignore) se não se verificar. */
+DROP TRIGGER IF EXISTS CheckUniqueTagNameWithinRepository;
+CREATE TRIGGER CheckUniqueTagNameWithinRepository
+BEFORE INSERT ON Tag
+WHEN 
+    NEW."name" IN (SELECT "name" FROM Tag)
+
+    AND
+
+    (SELECT repository
+    FROM Contribution
+    WHERE NEW."commit" = Contribution.ID)
+    IN
+    (SELECT repository
+     FROM Tag JOIN Contribution ON Tag."commit" = Contribution.ID) 
+
+BEGIN
+    SELECT RAISE(IGNORE);
+END;
