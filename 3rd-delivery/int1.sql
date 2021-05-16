243b5495d994fd3b4355fd4789391ef4bbde3142
beatriz-ag <beatrizaguiar.up@gmail.com>
2021-05-16 19:25:05 +0100

    int7 done SOMEONE PLS TEST IT I CANT NO MORE

--- 3rd-delivery/int1.sql
+++ 3rd-delivery/int1.sql
@@ -1,10 +1,7 @@
-SELECT Entity.name AS EntityName, Repository.name AS RepositoryName 
-FROM Entity  JOIN Repository ON Entity.ID = Repository."owner"
-ORDER BY Entity.name ASC;
+-- SELECT Entity.name AS EntityName, Repository.name AS RepositoryName 
+-- FROM Entity  JOIN Repository ON Entity.ID = Repository."owner"
+-- ORDER BY Entity.name ASC;
  
-
- -- eu acho que esta query devia ser a query atual das datas e na das datas fazer o tal cenas sequencial porque isto deixa de fazer sentido agora que o repositori tem o seu owner
-
 SELECT Repository.name AS RepositoryName, Entity.name AS EntityName, MIN(mostRecentContribution.date) AS ContributionDate 
 FROM Repository, Entity, (SELECT Contribution.user, Contribution.repository, MAX(Contribution.date) AS date
                           FROM Contribution
--- 3rd-delivery/int7.sql
+++ 3rd-delivery/int7.sql
@@ -1,6 +1,34 @@
-SELECT Repository.name AS RepositoryName, Entity.name AS EntityName, MIN(mostRecentContribution.date) AS ContributionDate 
-FROM Repository, Entity, (SELECT Contribution.user, Contribution.repository, MAX(Contribution.date) AS date
-                          FROM Contribution
-                          GROUP BY Contribution.user) AS mostRecentContribution
-WHERE repository.ID = mostRecentContribution.repository AND Entity.ID = mostRecentContribution.user;
- -- isto deixaria de existir [ler query 1]
+DROP VIEW IF EXISTS PopularRepos;
+CREATE VIEW PopularRepos AS
+    SELECT Repository.ID, Count(Contribution.id) as nrContributions
+    FROM Repository, Contribution
+    WHERE Repository.id = Contribution.repository
+    GROUP BY Repository.id
+    HAVING nrContributions > 1;
+
+
+
+DROP VIEW IF EXISTS ContributionsByUser;
+CREATE VIEW ContributionsByUser AS
+    SELECT DISTINCT Contribution.ID AS ID, Entity.name, Contribution.repository, Contribution.date
+    FROM Entity, Contribution, PopularRepos
+    WHERE Contribution.user = Entity.ID AND Contribution.repository = PopularRepos.id
+    GROUP BY Contribution.ID,Contribution.user, Contribution.repository
+    ORDER BY Contribution.user, Contribution.date;
+
+
+
+SELECT Repository.name, userName, max(MaxDaysWitoutContribution) as "DaysWindow"
+FROM
+    (SELECT userName, repo, max(TimeDifference) as MaxDaysWitoutContribution
+    FROM
+        (SELECT C1.name AS userName, C1.repository as repo, C1.date, C2.date, min(julianday(C2.date)-julianday(C1.date)) as TimeDifference
+        FROM ContributionsByUser AS C1, ContributionsByUser AS C2
+        WHERE C1.name = C2.name AND C1.date < C2.date AND C1.repository = C2.repository
+        GROUP BY C1.repository, c1.name)
+    GROUP BY repo, userName), Repository
+WHERE repo = Repository.ID
+GROUP BY repo;
+
+
+select * from ContributionsByUser;
