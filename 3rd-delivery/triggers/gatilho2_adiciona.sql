cb5be14b22de45be938469a96f178f270b1313ab
Margarida Vieira <margaridajcv.27@gmail.com>
2021-05-17 16:27:12 +0100

    gatilho2 almost finish

--- 3rd-delivery/triggers/gatilho2_adiciona.sql
+++ 3rd-delivery/triggers/gatilho2_adiciona.sql
@@ -1,3 +1,4 @@
+/*
 --  Só contribuidores de um repositório podem fazer uma contribuição para o mesmo.
 CREATE TRIGGER CheckContributionAvailabilityForRegularRepo
 BEFORE INSERT ON Contribution
@@ -18,7 +19,7 @@ WHEN
     NEW.user NOT IN (SELECT user FROM ContributorRepository WHERE repository = NEW.repository)
 BEGIN
     SELECT RAISE(FAIL, 'Regular Repository: a contribution must be made by the owner or by a contributor of the repository.');
-END;
+END;*/
 
 /*
 -- Só membros de uma organização podem fazer uma contribuição para um repositório da mesma. 
@@ -26,9 +27,7 @@ CREATE TRIGGER CheckContributionAvailabilityForTeamRepo
 BEFORE INSERT ON Contribution
 WHEN 
     -- team repository
-    (SELECT "onwer" FROM Repository WHERE New.repository = Repository.ID) 
-    IN 
-    (SELECT ID FROM Team)
+    NEW.repository IN (SELECT "owner" FROM Repository INTERSECT SELECT ID FROM Team)
 
     AND 
 
@@ -38,10 +37,11 @@ WHEN
     AND
 
     -- is not a member of the team to which the repository belongs
-    NEW.user NOT IN (SELECT user FROM TeamUserMember WHERE )
+    NEW.user NOT IN (SELECT user FROM TeamUserMember WHERE team = (SELECT "owner" FROM Repository WHERE ID = NEW.repository))
 BEGIN
     SELECT RAISE(FAIL, 'Team Repository: a contribution must be made by the owner or by a team member.');
 END;
+*/
 
 
 -- Só membros de uma equipa podem fazer uma contribuição para um repositório da mesma.  
@@ -49,9 +49,7 @@ CREATE TRIGGER CheckContributionAvailabilityForOrgRepo
 BEFORE INSERT ON Contribution
 WHEN 
     -- organization repository
-    (SELECT "onwer" FROM Repository WHERE New.repository = Repository.ID) 
-    IN 
-    (SELECT ID FROM Team)
+    NEW.repository IN (SELECT "owner" FROM Repository INTERSECT SELECT ID FROM Organization)
 
     AND 
 
@@ -61,8 +59,7 @@ WHEN
     AND
 
     -- is not a member of the organization to which the repository belongs
-    
+    -- finish
 BEGIN
     SELECT RAISE(FAIL, 'Organization Repository: a contribution must be made by the owner or by an organization member.');
 END;
-*/
